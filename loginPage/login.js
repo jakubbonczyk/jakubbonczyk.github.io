c344c76f2601d921594c28e56bcf2955b0528fbe
jakubbonczyk <139737489+jakubbonczyk@users.noreply.github.com>
2023-10-08 01:51:49 +0200

    Add files via upload

---
+++ loginPage/login.js
@@ -0,0 +1,11 @@
+const alert1 = document.querySelector('.resetpassword a');
+const alert2 = document.querySelector('button');
+const alert3 = document.querySelector('.register a')
+function getAlert() {
+    alert("Only for portfolio purposes.")
+}
+
+alert1.addEventListener("click", getAlert);
+alert2.addEventListener("click", getAlert);
+alert3.addEventListener("click", getAlert);
+
